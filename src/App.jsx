4bd630b110af930329671d8d8c166bb288a61abc
Shiva Neupane <shivaneupanexyz345@gmail.com>
2025-10-17 20:25:30 +0545

    do correction in imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,34 @@
 import "./App.css";
-import Card from "./components/card";
-import MovieData from "./data/Movies.json";
+import Card from "../components/Card"
 import Nabbar from "./Nabbar";
-import Footer from "./components/Footer.jsx";
+import MovieData from "../assets/data/Movies.json"
+import Footer from "../components/Footer"
 
 function App() {
   return (
-    <> 
- <div className="px-3">
-  <Nabbar/>
- </div>
-
-    <div className="flex flex-wrap gap-6 items-center justify-center">
-       
-      {MovieData.map((item, i) => {
-        return (
-          <Card
-            key={i}
-            name={item.name}
-            image={item.image}
-            releaseDate={item.releaseDate}
-            plot={item.plot}
-            
-          />
-        );
-      })}
-    </div>
-      
-      <div>
-        <Footer/>
+    <>
+      <div className="px-3">
+        <Nabbar />
       </div>
-  
- 
-</>
+
+      <div className="flex flex-wrap gap-6 items-center justify-center">
+        {MovieData.map((item, i) => {
+          return (
+            <Card
+              key={i}
+              name={item.name}
+              image={item.image}
+              releaseDate={item.releaseDate}
+              plot={item.plot}
+            />
+          );
+        })}
+      </div>
+
+      <div>
+        <Footer />
+      </div>
+    </>
   );
 }
 export default App;
