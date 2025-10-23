4329448de27155ba4c7aec9ddcc56de41f71ef76
“[Narayan <[neupanen627@gmail.com]>
2025-10-23 18:56:57 +0545

    Changes in UI

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,8 +1,8 @@
 const Card = (props) => {
   return (
     <>
-      <div className="  border-1 solid white w-70 h-130 bg-amber-400 shadow-lg shadow-indigo-500/50">
-        <img src={props.image} alt="Moviecard" className="h-90 w-70" />
+      <div className="  border-1 solid white w-70 h-125 bg-amber-400 shadow-lg shadow-indigo-500/50 p-3 rounded-2xl">
+        <img src={props.image} alt="Moviecard" className="h-70 w-70 rounded-2xl" />
         <h1>
          
           <b> Name</b>:{props.name}
@@ -14,9 +14,9 @@ const Card = (props) => {
 
           <b> Plot</b>:{props.plot}
         </p>
-       <div className="   ">
-          <div className="  flex justify-center items-center  bg-white  cursor-pointer  rounded-xl text-green-700 "> 
-          <a href="#" className="">  <button className=" cursor-pointer  flex justify-center items-center "> Download</button></a>
+       <div className="flex flex-col justify-end">
+          <div className="  flex justify-center items-center  bg-white  cursor-pointer  rounded-xl text-green-700 mb-2 "> 
+          <a href="#" className="">  <button className=" cursor-pointer flex justify-center items-center"> Download</button></a>
         </div>
         <div className="  flex justify-center items-center  bg-yellow-800  cursor-pointer  rounded-xl text-white"> 
           <a href="#" className="">  <button className=" cursor-pointer  flex justify-center items-center "> watch now</button></a>
